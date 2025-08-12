b90572635b695a14f970c7cd93ea97f2a1299e96
prajane <pramod.rajane@gmail.com>
2025-08-12 18:39:38 +0530

    Initial commit

---
+++ camunda-self-managed-localhost/src/main/java/com/camunda/selfmanaged/CamundaSelfManagedLocalhostApplication.java
@@ -0,0 +1,13 @@
+package com.camunda.selfmanaged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaSelfManagedLocalhostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaSelfManagedLocalhostApplication.class, args);
+	}
+
+}
---
+++ camunda-self-managed-localhost/src/test/java/com/camunda/selfmanaged/CamundaSelfManagedLocalhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.selfmanaged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaSelfManagedLocalhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
